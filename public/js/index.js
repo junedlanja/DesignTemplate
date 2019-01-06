eb82a6df7e20f4697cacfe59359f5a827cac62ee
junedlanja <junedlanja@gmail.com>
2019-01-06 16:24:54 +0530

    Fonts

--- public/js/index.js
+++ public/js/index.js
@@ -1,45 +1,45 @@
 $(document).ready(function () {
-    $("#loader").html("Loading...");
+	$("#loader").html("Loading...");
 
-    //var desingURL = 'designs/dino/Dino-Examples-01.svg';
-    //var desingURL = 'designs/test.svg';
-    var desingURL = 'designs/Dino_Standard_Label_20x50mm_Examples-01.svg';
+	//var desingURL = 'designs/dino/Dino-Examples-01.svg';
+	//var desingURL = 'designs/test.svg';
+	var desingURL = 'designs/Dino_Standard_Label_20x50mm_Examples-01.svg';
 
-    //intialize design template by creating canvas
-    DesignTemplate.createCanvas('canvas');
+	//intialize design template by creating canvas
+	DesignTemplate.createCanvas('canvas');
 
-    DesignTemplate.addDesign({
-    	url : desingURL,
-    	width : 268,
-    	height : 210
-    }, function(designInfo) {
-        $("#loader").html("Ready to desing !!!");
-    	var texts = designInfo.texts;
-    	for (var i = 0; i < texts.length; i++) {
-    		$("#design-text-container").append('<input id="' + (i+1) +'" class="design-text" type="text" value="' + texts[i] + '"/>');
-    	}
-    });
+	DesignTemplate.addDesign({
+		url: desingURL,
+		width: 268,
+		height: 210
+	}, function (designInfo) {
+		$("#loader").html("Ready to desing !!!");
+		var texts = designInfo.texts;
+		for (var i = 0; i < texts.length; i++) {
+			$("#design-text-container").append('<input id="' + (i + 1) + '" class="design-text" type="text" value="' + texts[i] + '"/>');
+		}
+	});
 
-    $("#design-text-container").on("input", ".design-text", function(event){
-    	var target = $(event.target);
-    	var index = target.attr("id");
-    	var value = target.val();
-    	DesignTemplate.changeText(index, value);
-    });
+	$("#design-text-container").on("input", ".design-text", function (event) {
+		var target = $(event.target);
+		var index = target.attr("id");
+		var value = target.val();
+		DesignTemplate.changeText(index, value);
+	});
 
-    $("#design-text-container").on("input", ".design-text", function(event){
-    	var target = $(event.target);
-    	var index = target.attr("id");
-    	var value = target.val();
-    	DesignTemplate.changeText(index, value);
-    });
+	$("#design-text-container").on("input", ".design-text", function (event) {
+		var target = $(event.target);
+		var index = target.attr("id");
+		var value = target.val();
+		DesignTemplate.changeText(index, value);
+	});
 
-    $("#font-family").on("change", function(event){
-    	var fontFamily = this.value;
-    	loadFonts(fontFamily, function(){
-    		DesignTemplate.changeFontFamily(fontFamily);
-    	}, function(){
-    		alert('Unable to load fonts');
-    	});
-    })
+	$("#font-family").on("change", function (event) {
+		var fontFamily = this.value;
+		loadFonts(fontFamily, function () {
+			DesignTemplate.changeFontFamily(fontFamily);
+		}, function () {
+			alert('Unable to load fonts');
+		});
+	})
 })
