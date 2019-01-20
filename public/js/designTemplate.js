20122dd7de844408e5522495fb907f7826faf34e
junedlanja <junedlanja@gmail.com>
2019-01-20 22:33:04 +0530

    Text resize & Color feature

--- public/js/designTemplate.js
+++ public/js/designTemplate.js
@@ -6,7 +6,7 @@ fabric.Object.prototype.set({
 });
 
 //Design template module
-var DesignTemplate = (function() {
+var DesignTemplate = (function () {
     return {
         canvas: null,
 
@@ -14,12 +14,29 @@ var DesignTemplate = (function() {
 
         defaultWidth: 600,
 
+        maxTextLength: 25,
+
+        maxTextWidth: 0,
+
+        maxTextHeight: 0,
+
         designTexts: [],
 
-        designFontFamily: "American Purpose",
+        textObjectInfo: [],
+
+        designFontFamily: "Avenir",
+
+        getMaxDimension: function () {
+            this.maxTextWidth = Math.max.apply(null, this.textObjectInfo.map(function (obj) {
+                return obj.width;
+            }));
+            this.maxTextHeight = Math.max.apply(null, this.textObjectInfo.map(function (obj) {
+                return obj.height;
+            }));
+        },
 
         //creates the canvas with specified height and width
-        createCanvas: function(canvasId) {
+        createCanvas: function (canvasId) {
             if (!canvasId || !document.getElementById(canvasId))
                 throw new Error('Canvas element not found');
             this.canvas = new fabric.StaticCanvas(canvasId, {
@@ -31,65 +48,106 @@ var DesignTemplate = (function() {
             return this.canvas;
         },
 
-        addDesign: function(options, callback) {
+        addDesign: function (options, callback) {
             var self = this;
-            fabric.loadSVGFromURL(options.url, function(objects) {
-                objects.filter(function(obj) {
+            fabric.loadSVGFromURL(options.url, function (objects) {
+                objects.filter(function (obj) {
                     if (obj.type === "text") {
                         var index = self.designTexts.indexOf(obj.text);
                         if (index == -1) {
                             obj.id = self.designTexts.length + 1;
                             self.designFontFamily = obj.fontFamily;
                             self.designTexts.push(obj.text);
-                        }else {
+                        } else {
                             obj.id = index + 1;
                         }
+                        self.textObjectInfo.push({
+                            canvasObj: obj,
+                            width: obj.width,
+                            height: obj.height,
+                            scaleX: obj.scaleX,
+                            scaleY: obj.scaleY,
+                            fill: obj.fill,
+                            stroke: obj.stroke
+                        });
                     }
                 });
-              loadFonts(self.designFontFamily, function() {
-                var group = fabric.util.groupSVGElements(objects, {
-                    width: options.width,
-                    height: options.height,
-                    selectable: false
+                loadFonts(self.designFontFamily, function () {
+                    var group = fabric.util.groupSVGElements(objects, {
+                        width: options.width,
+                        height: options.height,
+                        selectable: false
+                    });
+                    (options.height >= options.width) ? group.scaleToHeight(self.canvas.getHeight()): group.scaleToWidth(self.canvas.getWidth());
+                    self.canvas.centerObject(group);
+                    self.canvas.add(group);
+                    self.canvas.renderAll();
+                    self.getMaxDimension();
+                    callback && callback({
+                        fontFamily: self.designFontFamily,
+                        texts: self.designTexts,
+                        textObjectInfo: self.textObjectInfo
+                    });
+                }, function () {
+                    alert('Unable to load fonts');
                 });
-                (options.height >= options.width) ? group.scaleToHeight(self.canvas.getHeight()): group.scaleToWidth(self.canvas.getWidth());
-                self.canvas.centerObject(group);
-                self.canvas.add(group);
+            });
+        },
+
+        changeText: function (id, text) {
+            var group = this.canvas.getObjects()[0];
+            // group.getObjects().filter(function (obj) {
+            //     if (obj.type == "text" && obj.id == id) {
+            //         obj.text = text;
+            //     }
+            // });
+            this.textObjectInfo.filter(function (obj) {
+                obj.canvasObj.id == id && (obj.canvasObj.text = text);
+            });
+            group.set('dirty', true);
+            this.canvas.renderAll();
+            this.resizeText();
+        },
+
+        changeFontFamily: function (fontFamily) {
+            var group = this.canvas.getObjects()[0];
+            // group.getObjects().filter(function (obj) {
+            //     if (obj.type == "text") {
+            //         obj.fontFamily = fontFamily;
+            //     }
+            // });
+            this.textObjectInfo.filter(function (obj) {
+                obj.canvasObj.fontFamily = fontFamily;
+            });
+            group.set('dirty', true);
+            this.canvas.renderAll();
+            this.resizeText();
+        },
+
+        changeColor: function (id, color) {
+            var group = this.canvas.getObjects()[0];
+            this.textObjectInfo[id].canvasObj.fill = color;
+            group.set('dirty', true);
+            this.canvas.renderAll();
+        },
+
+        resizeText: function () {
+            var self = this;
+            var changed = true;
+            this.textObjectInfo.filter(function (obj) {
+                var width = obj.canvasObj.width;
+                var ratio = self.maxTextWidth / width;
+                if (width > self.maxTextWidth) {
+                    changed = true;
+                    obj.canvasObj.scaleX = ratio * obj.scaleX;
+                    obj.canvasObj.scaleY = ratio * obj.scaleY;
+                }
+            });
+            if (changed) {
+                var group = self.canvas.getObjects()[0];
+                group.set('dirty', true);
                 self.canvas.renderAll();
-                callback && callback({
-                    fontFamily: self.designFontFamily,
-                    texts: self.designTexts
-                });
-              }, function() {
-                  alert('Unable to load fonts');
-              });
-            });
-        },
-
-        changeText: function(id, text) {
-            var group = this.canvas.getObjects()[0];
-            group.getObjects().filter(function(obj) {
-                if (obj.type == "text" && obj.id == id) {
-                    obj.text = text;
-                }
-            });
-            group.set('dirty', true);
-            this.canvas.renderAll();
-        },
-
-        changeFontFamily: function(fontFamily) {
-            var group = this.canvas.getObjects()[0];
-            group.getObjects().filter(function(obj) {
-                if (obj.type == "text") {
-                    obj.fontFamily = fontFamily;
-                }
-            });
-            group.set('dirty', true);
-            this.canvas.renderAll();
+            }
         }
-
-
-
-
     }
 })();
--- public/js/index.js
+++ public/js/index.js
@@ -10,14 +10,21 @@ $(document).ready(function () {
 
 	DesignTemplate.addDesign({
 		url: desingURL,
-		width: 268,
-		height: 210
+		// width: 268,
+		// height: 210
 	}, function (designInfo) {
 		$("#loader").html("Ready to desing !!!");
 		var texts = designInfo.texts;
+		var colors = designInfo.textObjectInfo.map(function (obj) {
+			return obj.fill;
+		});
 		for (var i = 0; i < texts.length; i++) {
-			$("#design-text-container").append('<input id="' + (i + 1) + '" class="design-text" type="text" value="' + texts[i] + '"/>');
+			$("#design-text-container").append('<input id="' + (i + 1) + '" class="design-text" type="text" max-length="' + DesignTemplate.maxTextLength + '" value="' + texts[i] + '"/>');
 		}
+		for (var i = 0; i < colors.length; i++) {
+			$("#design-color-container").append('<span id="color-' + (i + 1) + '" data-id="' + i + '" class="design-color" style="background-color:' + colors[i] + '"/>');
+		}
+		$('.design-color').colorpicker();
 	});
 
 	$("#design-text-container").on("input", ".design-text", function (event) {
@@ -27,19 +34,22 @@ $(document).ready(function () {
 		DesignTemplate.changeText(index, value);
 	});
 
-	$("#design-text-container").on("input", ".design-text", function (event) {
-		var target = $(event.target);
-		var index = target.attr("id");
-		var value = target.val();
-		DesignTemplate.changeText(index, value);
-	});
 
 	$("#font-family").on("change", function (event) {
 		var fontFamily = this.value;
+		$("#font-family").css("font-family", fontFamily);
 		loadFonts(fontFamily, function () {
 			DesignTemplate.changeFontFamily(fontFamily);
 		}, function () {
 			alert('Unable to load fonts');
 		});
-	})
+	});
+
+	$('#design-color-container').on('changeColor', '.design-color', function (event) {
+		var ele = $(event.target);
+		var id = ele.data("id");
+		var color = event.color.toString();
+		$("#" + ele.attr("id")).css("background-color", color);
+		DesignTemplate.changeColor(id, color);
+	});
 })
