42b2148a21b9729da791ef11852b64638bc78e6f
junedlanja <junedlanja@gmail.com>
2019-01-25 22:56:32 +0530

    Optimized SVG

--- public/js/index.js
+++ public/js/index.js
@@ -1,62 +1,67 @@
 function renderTextOptions() {
-	$("#design-text-container").empty();
-	var texts = DesignTemplate.designTexts;
-	for (var i = 0; i < texts.length; i++) {
-		$("#design-text-container").append('<input id="' + (i + 1) + '" class="design-text" type="text" max-length="' + DesignTemplate.maxTextLength + '" value="' + texts[i] + '"/>');
-	}
+    $("#design-text-container").empty();
+    var texts = DesignTemplate.designTexts;
+    for (var i = 0; i < texts.length; i++) {
+        $("#design-text-container").append('<input id="' + (i + 1) + '" class="design-text" type="text" max-length="' + DesignTemplate.maxTextLength + '" value="' + texts[i] + '"/>');
+    }
 }
 
 function renderColorOptions() {
-	$("#design-color-container").empty();
-	var colors = DesignTemplate.textObjectInfo.map(function (obj) {
-		return obj.fill;
-	});
-	for (var i = 0; i < colors.length; i++) {
-		$("#design-color-container").append('<span id="color-' + (i + 1) + '" data-id="' + i + '" class="design-color" style="background-color:' + colors[i] + '"/>');
-	}
-	$('.design-color').colorpicker();
+    $("#design-color-container").empty();
+    var colors = DesignTemplate.textObjectInfo.map(function (obj) {
+        return obj.fill;
+    });
+    for (var i = 0; i < colors.length; i++) {
+        $("#design-color-container").append('<span id="color-' + (i + 1) + '" data-id="' + i + '" class="design-color" style="background-color:' + colors[i] + '"/>');
+    }
+    $(".design-color").colorpicker();
+}
+
+
+function selectDefaultFontFamily() {
+    var defaultFontFamily = DesignTemplate.designFontFamily;
+    $('#font-family option[value="' + defaultFontFamily + '"]').prop("selected", true);
 }
 
 $(document).ready(function () {
-	$("#loader").html("Loading...");
+    $("#loader").html("Loading...");
 
-	//var desingURL = 'designs/dino/Dino-Examples-01.svg';
-	//var desingURL = 'designs/test.svg';
-	var desingURL = 'designs/Dino_Standard_Label_20x50mm_Examples-01.svg';
+    //var designURL = 'designs/Dino_Standard_Label_20x50mm_Examples-01.svg';
+    var designURL = "designs/Dino_Optimized.svg";
 
-	DesignTemplate.createCanvas('canvas');
+    DesignTemplate.createCanvas("canvas");
 
-	DesignTemplate.addDesign({
-		url: desingURL,
-	}, function () {
-		$("#loader").html("Ready to desing !!!");
-		renderTextOptions();
-		renderColorOptions();
-	});
+    DesignTemplate.addDesign({
+        url: designURL
+    }, function () {
+        $("#loader").html("Ready to desing !!!");
+        renderTextOptions();
+        renderColorOptions();
+        selectDefaultFontFamily();
+    });
 
-	$("#design-text-container").on("input", ".design-text", function (event) {
-		var target = $(event.target);
-		var index = target.attr("id");
-		var value = target.val();
-		DesignTemplate.changeText(index, value);
-	});
+    $("#design-text-container").on("input", ".design-text", function (event) {
+        var target = $(event.target);
+        var index = target.attr("id");
+        var value = target.val();
+        DesignTemplate.changeText(index, value);
+    });
 
+    $("#font-family").on("change", function (event) {
+        var fontFamily = this.value;
+        $("#font-family").css("font-family", fontFamily);
+        loadFonts(fontFamily, function () {
+            DesignTemplate.changeFontFamily(fontFamily, renderTextOptions);
+        }, function () {
+            alert("Unable to load fonts");
+        });
+    });
 
-	$("#font-family").on("change", function (event) {
-		var fontFamily = this.value;
-		$("#font-family").css("font-family", fontFamily);
-		loadFonts(fontFamily, function () {
-			DesignTemplate.changeFontFamily(fontFamily, renderTextOptions);
-		}, function () {
-			alert('Unable to load fonts');
-		});
-	});
-
-	$('#design-color-container').on('changeColor', '.design-color', function (event) {
-		var ele = $(event.target);
-		var id = ele.data("id");
-		var color = event.color.toString();
-		$("#" + ele.attr("id")).css("background-color", color);
-		DesignTemplate.changeColor(id, color);
-	});
-})
+    $("#design-color-container").on("changeColor", ".design-color", function (event) {
+        var ele = $(event.target);
+        var id = ele.data("id");
+        var color = event.color.toString();
+        $("#" + ele.attr("id")).css("background-color", color);
+        DesignTemplate.changeColor(id, color);
+    });
+});
