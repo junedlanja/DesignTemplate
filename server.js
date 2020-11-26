70dc4ed45a9fc98d306dfa829703830eb54508ce
junedlanja <junedlanja@gmail.com>
2020-11-26 16:10:32 +0530

    Port from .env

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var express = require('express'),
     app = express();
 
 //set the port
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 
 //tell express that we want to use the public folder
 //for our static assets
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Listen for requests
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(app.get('port'), function () {
     console.log('The server is running on http://localhost:' + app.get('port'));
 });
